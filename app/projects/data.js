53a5f44d1065e5c3416aaf4af7e31154ca82df9f
MercutioG <110576904+MercutioG@users.noreply.github.com>
2024-03-26 15:19:20 -0700

    Updated data.js

--- app/projects/data.js
+++ app/projects/data.js
@@ -1,15 +1,89 @@
 const data = [{
     "id": 1,
-    "title": "My First Webpage",
+    "title": "Bernard's Burritos",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
     "image": "https://res.cloudinary.com/dbifof9on/image/upload",
-    "url": "https://github.com/"
+    "url": "https://mcarri355.github.io/Website-Project/",
+    "github": "https://github.com/mcarri355/Website-Project"
   },
   {
     "id": 2,
-    "title": "My First Webpage",
+    "title": "Number Converter",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
     "image": "https://res.cloudinary.com/dbifof9on/image/upload",
-    "url": "https://github.com/"
-  }
+    "url": "https://mercutiog.github.io/Number-Converter/",
+    "github": "https://github.com/MercutioG/Number-Converter"
+  },
+  {
+    "id": 3,
+    "title": "Pizza Order Form",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/pizza-form/",
+    "github": "https://github.com/MercutioG/pizza-form"
+  },
+  {
+    "id": 4,
+    "title": "Picture Grid",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/picture-grid/",
+    "github": "https://github.com/MercutioG/picture-grid"
+  },
+  {
+    "id": 5,
+    "title": "Bernard's Breakfast",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://ethantransier.github.io/End-of-unit-1-project/",
+    "github": "https://github.com/EthanTransier/End-of-unit-1-project"
+  },
+  {
+    "id": 6,
+    "title": "Basic Calculator",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/basic-calculator/",
+    "github": "https://github.com/MercutioG/basic-calculator"
+  },
+  {
+    "id": 7,
+    "title": "My First Portfolio",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/portfolio/index.html",
+    "github": "https://github.com/MercutioG/portfolio"
+  },
+  {
+    "id": 8,
+    "title": "Rigged Nixon Election",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/rigNixonElection/",
+    "github": "https://github.com/MercutioG/rigNixonElection"
+  },
+  {
+    "id": 9,
+    "title": "JQuery Social Media Page",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/jQuery-Media-Page/",
+    "github": "https://github.com/MercutioG/jQuery-Media-Page"
+  },
+  {
+    "id": 10,
+    "title": "Summer Website",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/Summer-Website/",
+    "github": "https://github.com/MercutioG/jQuery-Media-Page"
+  },
+  {
+    "id": 11,
+    "title": "Summer Website",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat et justo vit autem quibusdam quidem quasi, quod, quaerat et justo verit. Lorem ipsum dolor sit amet, con sect et justo tempor nisi.",
+    "image": "https://res.cloudinary.com/dbifof9on/image/upload",
+    "url": "https://mercutiog.github.io/Summer-Website/",
+    "github": "https://github.com/MercutioG/jQuery-Media-Page"
+  },
 ]
